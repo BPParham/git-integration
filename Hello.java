c29cc1aba0755cfe1e98790a333d16c902d70132
bobbypparham <bpparham@yahoo.com>
2017-10-19 09:00:33 -0500

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  public static void main(String[] args){
+    for(int i=0; i<10; i++){
+      if(i==0){
+        System.out.println("Hello World... " + (i+1) + " time!");
+      } else {
+        System.out.println("Hello World... " + (i+1) + " times!");
+      }
+    }
+  }
+}
